1119c3670efa52fb480a43539134c1cbcecd1d11
nachomaffei7 <118315534+nachomaffei7@users.noreply.github.com>
2022-12-04 23:28:40 -0300

    Ejercitacion 11

---
+++ Ejercitacion 11/js11.js
@@ -0,0 +1,47 @@
+let diaSemana = prompt("¿Qué día es hoy?");
+switch (diaSemana) {
+case "lunes":
+
+alert("Hoy toca pecho.");
+
+break;
+case "martes":
+
+alert("Hoy toca piernas.");
+
+break;
+case "miércoles":
+
+alert("Miercoles de descanso.");
+
+break;
+case "jueves":
+
+alert("Hoy toca cardio: 15 km bicicleta.");
+
+break;
+case "viernes":
+
+alert("Hoy es dia de espalda.");
+
+break;
+case "sábado":
+
+alert("Hoy toca caminata tranquila de 5km.");
+
+break;
+case "domingo":
+
+alert("Domingo de futbol con amigos + permitido.");
+
+break;
+default:
+
+alert("Escribe el día de la semana en minúsculas.");
+
+}
+for(i=20;i<=70;i++){
+
+document.write("El número es: " + i + "<br>");
+
+}
