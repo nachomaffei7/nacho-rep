9d0bb08f66e0c573e817b58507cffda5100b6895
nachomaffei7 <118315534+nachomaffei7@users.noreply.github.com>
2022-12-15 18:09:31 -0300

    Entrega Final
    Sitio web Estudio Jurídico

---
+++ Proyecto final/index.js
@@ -0,0 +1,7 @@
+let nombre = prompt ("Indicanos tu nombre")
+
+let cliente = prompt (nombre+", sos cliente del estudio?")
+
+if (cliente == 'si'||cliente == 'Si')
+alert("Bienvenido "+nombre)
+else alert("Contactanos para realizar tu consulta, estamos a tu disposición")
